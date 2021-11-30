064767ed1f20537ee3f37a36e1978b58272dc4fa
Seoyoung Lee <selee@student.42lyon.fr>
2021-11-30 13:56:01 +0100

    deleted time.c file (moved the function into utils.c)

--- philo/srcs/utils.c
+++ philo/srcs/utils.c
@@ -6,11 +6,22 @@
 /*   By: selee <selee@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 10:21:09 by selee             #+#    #+#             */
-/*   Updated: 2021/11/30 13:51:20 by selee            ###   ########lyon.fr   */
+/*   Updated: 2021/11/30 13:54:30 by selee            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+long	get_time_in_ms(void)
+{
+	struct timeval	tp;
+	long			ret;
+
+	if (gettimeofday(&tp, NULL) < 0)
+		return (ERROR);
+	ret = (long)tp.tv_sec * 1000 + (long)tp.tv_usec / 1000;
+	return (ret);
+}
 
 int	print_status(t_philo *philo, char *str, int in_main)
 {
